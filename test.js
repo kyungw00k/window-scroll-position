ff6617134e9b99fee8f49563d42591c441519cfe
Ryan Burnette <ryan.burnette@gmail.com>
2018-03-14 19:30:41 -0400

    change test

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ var windowScrollPosition = require('./')
 
 test('window', function (t) {
   document.body.style.position = 'relative'
-  document.body.style.width = '31000px'
-  document.body.style.height = '31000px'
+  document.body.style.width = '100px'
+  document.body.style.height = '100px'
 
-  window.scrollTo(30000, 30000)
+  window.scrollTo(50,50)
 
   t.deepEqual(windowScrollPosition(window), {
     left: document.documentElement.scrollWidth - window.innerWidth,
